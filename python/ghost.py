0d9dee68a39a0b038103fe202b680bbd0924ae94
cryptomirror <cryptomirror@gmail.com>
2013-11-26 20:37:55 -0800

    Update ghost example

--- python/ghost.py
+++ python/ghost.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 import message
+import nacl
 
-ID_LEN = message.nacl.STREAM_KEYSIZE
+ID_LEN = nacl.STREAM_KEYSIZE
+PK_LEN = nacl.PUBLICKEYBYTES
+PN_LEN = nacl.STREAM_NONCEBYTES
 
 def gen_id(size=ID_LEN):
-  nonce = message.nacl.BUF(size)
-  ret = message.nacl.LIB.randombytes(nonce, size)
+  nonce = nacl.BUF(size)
+  ret = nacl.LIB.randombytes(nonce, size)
   if ret != size:
     raise Exception("Expected random bytes: %d"%size)
   return nonce[:]
@@ -20,13 +23,132 @@ def read_key(name):
 
   return pk1, sk1
   
+def create_anon_channel_pair():
+  """
+  Create a keypair and an ID for an anonymous ECDH channel
+  """
+  #create alice's anon pairing and ID for alice->bob
+  anon_pk1, anon_sk1 = message.make_keypair()
+  ID_to_send = gen_id()
+  return anon_pk1, anon_sk1, ID_to_send
+
+def send_identity_over_channel(channel_sender_pk,
+                               channel_sender_sk,
+                               channel_receiver_pk,
+                               sender_pk,
+                               nickname):
+  """
+  Create a pubkey + nickname message over the anon ECDH channel
+  """
+  #alice sends bob true pubkey and ID key
+  stage2 = sender_pk + nickname
+
+  stage1 = message.create_encrypted_message("",
+                                            channel_sender_pk,
+                                            channel_sender_sk,
+                                            channel_receiver_pk,
+                                            stage2)
+  return stage1
+
+def get_identity_from_channel(stage1,
+                              channel_sk):
+  """
+  Extract nickname and pubkey from an identity channel
+  """
+  #alice decrypts anon stage 1 from bob
+  s_nick, s_pub, r_pub, nonce, cipher = message.parse_message(stage1)
+  stage2_plain = nacl.nacl_decrypt(s_pub, channel_sk, nonce, cipher)
+  
+  sender_pub = stage2_plain[:PK_LEN]
+  known_nick = stage2_plain[PK_LEN + ID_LEN:]
+  return sender_pub, known_nick
+
+def send_id_key_over_channel(channel_sender_pk,
+                             channel_sender_sk,
+                             channel_receiver_pk,
+                             sender_pk,
+                             sender_sk,
+                             receiver_pk,
+                             nickname,
+                             ID):
+  """
+  Send Secret ID key over persistent keys over ECDH channel
+  Note: This ID is unique for this communication pairing.
+  """
+  #alice sends bob true pubkey and ID key
+  stage2 = message.create_encrypted_message(nickname,
+                                            sender_pk,
+                                            sender_sk,
+                                            receiver_pk,
+                                            ID + nickname)
+
+  stage1 = message.create_encrypted_message("",
+                                            channel_sender_pk,
+                                            channel_sender_sk,
+                                            channel_receiver_pk,
+                                            stage2)
+  return stage1
+
+def get_id_key_from_channel(stage1,
+                            channel_sk,
+                            receiver_sk):
+  """
+  Extract an id key and nickname from an id key message
+  """
+  #bob decrypts anon stage 1 from alice
+  s_nick, s_pub, r_pub, nonce, cipher = message.parse_message(stage1)
+  stage2_plain = nacl.nacl_decrypt(s_pub, channel_sk, nonce, cipher)
+  
+  real_nick, sender_pub, my_pub, nonce, cipher = message.parse_message(stage2_plain)
+  ID_alice = nacl.nacl_decrypt(sender_pub, receiver_sk, nonce, cipher)
+  new_id = ID_alice[:ID_LEN]
+  nick = ID_alice[ID_LEN:]
+  return new_id, nick
+
+def send_ghost_message(target_id,
+                       sender_sk,
+                       receiver_pk,
+                       msg):  
+  """
+  Send a message over the ECDH established by the identity-key pairings,
+  and a nonce + challenge answer using the Secret ID key of the target  
+  """
+  poly_nonce, ciphertext = nacl.nacl_encrypt(sender_sk,
+                                             receiver_pk,
+                                             msg)
+  nonce, f_nonce = nacl.nacl_symmetric(8, target_id)
+  return nonce, f_nonce, poly_nonce + ciphertext
+
+def verify_sender(nonce,
+                  answer,
+                  id_to_verify):
+  """
+  Verify the nonce and answer with the a given id
+  """
+  n2, f1_nonce = nacl.nacl_symmetric(8, id_to_verify, nonce)
+  if answer == f1_nonce:
+    return True
+  else:
+    return False  
+
+def get_ghost_message(sender_pk,
+                      receiver_sk,
+                      nonce,
+                      answer,
+                      ciphertext):
+  """
+  Decrypt a message from the known sender
+  """
+  poly_nonce, ciphertext = ciphertext[:PN_LEN], ciphertext[PN_LEN:]
+  plaintext = nacl.nacl_decrypt(sender_pk, receiver_sk, poly_nonce, ciphertext)
+  return plaintext
 
 """
 0. Two parties have keys
 
 Initial contact between A and B
 1. Set up anonymous ECDH channel
-2. Generate identity keys
+2. Transmit nicknames and pub keys through channel
 3. Transmit public keys, nicknames, and secret IDs over anonymous ECDH channel
 
 The Secret ID can be thought of as an edge between A and B, it is a unique
@@ -39,10 +161,11 @@ Subsequent communication between A and B
     then if match->decrypts msg
 
 Q: why not just have receiver decrypt everything? maybe not a bad idea...
-    
-    
 """ 
 if __name__ == "__main__":
+  
+  #0. Pub keys are established
+  
   #these are the persistent identities
   alice_pk1, alice_sk1 = read_key("alice")
   alice_name = "alice"
@@ -50,63 +173,71 @@ if __name__ == "__main__":
   bob_pk2, bob_sk2 = read_key("bob")
   bob_name = "bob"
   
+  #1. Set up anonymous ECDH channel
+
   #create alice's anon pairing and ID for alice->bob
-  anon_pk1, anon_sk1 = message.make_keypair()
-  ID_alice_to_bob = gen_id()
+  anon_pk1, anon_sk1, ID_alice_to_bob = create_anon_channel_pair()
 
   #create bob's anon pairing and ID for bob->alice
-  anon_pk2, anon_sk2 = message.make_keypair()
-  ID_bob_to_alice = gen_id()
+  anon_pk2, anon_sk2, ID_bob_to_alice = create_anon_channel_pair()
   
   ### somehow anon_pk1 & anon_pk2 are exchanged ###
   
+  # 2. Transmit nicknames and pub keys through channel
+  
   #alice sends bob true pubkey and ID key
-  stage2_0 = message.create_encrypted_message(alice_name,
-                                              alice_pk1, 
-                                              alice_sk1, 
-                                              bob_pk2, 
-                                              ID_alice_to_bob + alice_name)
-
-  stage1_2 = message.create_encrypted_message("",
-                                              anon_pk1,
-                                              anon_sk1,
-                                              anon_pk2,
-                                              stage2_0)
+  stage1_0 = send_identity_over_channel(anon_pk1,
+                                        anon_sk1,
+                                        anon_pk2,
+                                        alice_pk1,
+                                        alice_name)
 
   #bob sends alice true pubkey and ID key
-  stage2_1 = message.create_encrypted_message(bob_name,
-                                              bob_pk2, 
-                                              bob_sk2, 
-                                              alice_pk1, 
-                                              ID_bob_to_alice + bob_name)
+  stage1_1 = send_identity_over_channel(anon_pk2,
+                                        anon_sk2,
+                                        anon_pk1,
+                                        bob_pk2,
+                                        bob_name)
 
-  stage1_3 = message.create_encrypted_message("",
-                                              anon_pk2,
-                                              anon_sk2,
-                                              anon_pk1,
-                                              stage2_1)
+  ### somehow stage1_0 and stage 1_1 are exchanged ###
 
-  ### somehow stage1_2 and stage1_3 are exchanged
-  
+  #3. Transmit public keys, nicknames, and secret IDs over anonymous ECDH channel
+
   #alice decrypts anon stage 1 from bob
-  s_nick, s_pub, r_pub, nonce, cipher = message.parse_message(stage1_3)
-  stage2_plain = message.nacl.nacl_decrypt(s_pub, anon_sk1, nonce, cipher)
-  real_nick, sender_pub, my_pub, nonce, cipher = message.parse_message(stage2_plain)
-  assert(my_pub == alice_pk1)
-  ID_bob = message.nacl.nacl_decrypt(sender_pub, alice_sk1, nonce, cipher)
-  alice_knows_bobs_id = ID_bob[:ID_LEN]
-  alice_knows_bobs_nick = ID_bob[ID_LEN:]
-  assert(real_nick == alice_knows_bobs_nick)
+  bob_pub, bob_nick = get_identity_from_channel(stage1_1, anon_sk1)
 
-  #bob decrypts anon stage 1 from alice
-  s_nick, s_pub, r_pub, nonce, cipher = message.parse_message(stage1_2)
-  stage2_plain = message.nacl.nacl_decrypt(s_pub, anon_sk2, nonce, cipher)
-  real_nick, sender_pub, my_pub, nonce, cipher = message.parse_message(stage2_plain)
-  assert(my_pub == bob_pk2)
-  ID_alice = message.nacl.nacl_decrypt(sender_pub, bob_sk2, nonce, cipher)
-  bob_knows_alices_id = ID_alice[:ID_LEN]
-  bob_knows_alices_nick = ID_alice[ID_LEN:]
-  assert(real_nick == bob_knows_alices_nick)
+  #bob does the same
+  alice_pub, alice_nick = get_identity_from_channel(stage1_0, anon_sk2)
+  
+  #alice sends bob ID key over channel + pub key
+  stage1_2 = send_id_key_over_channel(anon_pk1,
+                                      anon_sk1,
+                                      anon_pk2,
+                                      alice_pk1,
+                                      alice_sk1,
+                                      bob_pk2,
+                                      alice_name,
+                                      ID_alice_to_bob)
+                                      
+  #bob sends alice ID key over channel + pub key
+  stage1_3 = send_id_key_over_channel(anon_pk2,
+                                      anon_sk2,
+                                      anon_pk1,
+                                      bob_pk2,
+                                      bob_sk2,
+                                      alice_pk1,
+                                      bob_name,
+                                      ID_bob_to_alice)
+                                      
+  ### somehow stage1_2 and stage1_3 are exchanged  
+  
+  #alice gets id from bob
+  result = get_id_key_from_channel(stage1_3, anon_sk1, alice_sk1)
+  alice_knows_bobs_id, alice_knows_bobs_nick = result
+  
+  #bob gets id from alice
+  result = get_id_key_from_channel(stage1_3, anon_sk1, alice_sk1)
+  bob_knows_alices_id, bob_knows_alices_nick = result
 
   ###
   ### At this point Alice and Bob know each others IDs. These
@@ -114,25 +245,26 @@ if __name__ == "__main__":
   ###
   
   # Alice posts a message to bob
-  poly_nonce, ciphertext = message.nacl.nacl_encrypt(alice_sk1,
-                                                     bob_pk2,
-                                                     "Hello anonymous world")
-  nonce, f_nonce = message.nacl.nacl_symmetric(8, alice_knows_bobs_id)
+  nonce, f_nonce, ciphertext = send_ghost_message(alice_knows_bobs_id,
+                                                  alice_sk1, 
+                                                  bob_pk2, 
+                                                  "Hello anonymous world")  
   
-
-  # bob verifies f_nonce and decrypts message
+  identities = {ID_bob_to_alice: alice_pk1}
+  for ident in identities.keys():
+    print ident, k  ey
   # sidenote: @ 1,000,000 users this becomes too slow. 
   # a server would want to partition users by generating
-  # multiple identities to hand , and cross-signing the
+  # multiple identities to hand, and cross-signing the
   # identities.
   #
-  n2, f1_nonce = message.nacl.nacl_symmetric(8, ID_bob_to_alice, nonce)
-  if f_nonce == f1_nonce:
+  # bob verifies f_nonce and decrypts message
+  result = verify_sender(nonce, f_nonce, ID_bob_to_alice)
+  if result:
     pass
   else:
-    raise Exception("mismatch")
-  
-  plaintext = message.nacl.nacl_decrypt(alice_pk1, bob_sk2, poly_nonce, ciphertext)
+    raise Exception("Unknown sender")
+  plaintext = get_ghost_message(alice_pk1, bob_sk2, nonce, f_nonce, ciphertext)
   print plaintext
   
   
